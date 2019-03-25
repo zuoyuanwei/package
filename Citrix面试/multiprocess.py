69ee865e631cd206136e500e3869d7884548a2f4
zuo <1439933747@qq.com>
2019-03-25 21:04:10 +0800

    Citrix面试

---
+++ Citrix面试/multiprocess.py
@@ -0,0 +1,20 @@
+# windows创建多个进程
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)		# pool默认大小为电脑cpu核心数，参数至少为核心数+1才能看见等待效果，否则多进程执行看不见等待效果。
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')

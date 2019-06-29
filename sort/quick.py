76c4f94ef16208c6dd6ca12ab4919a2f471a3ebe
zuoyuanwei <42493555+zuoyuanwei@users.noreply.github.com>
2019-06-29 09:55:15 +0800

    Update quick.py

--- sort/quick.py
+++ sort/quick.py
@@ -2,14 +2,14 @@
 # 数据越随机分布时，快速排序性能越好；数据越接近有序，快速排序性能越差。
 class QuickSort:
     def Quick(self, array, low, high):
-        if low < high:
+        if low < high:  # 定义左右指针，当左右指针不重合时才迭代排序
             key_index = self.Partion(array, low, high)
             self.Quick(array, low, key_index)
             self.Quick(array, key_index + 1, high)
         return array
 
     def Partion(self, array, low, high):
-        key = array[low]
+        key = array[low]    # base元素定义为左指针指向的元素
         while low < high:
             while low < high and array[high] >= key:
                 high -= 1
